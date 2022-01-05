62c5d1f07ed7e985509753887f615f8999e25cb0
KatmannikCore <vlad.urbanovitch@yandex.by>
2022-01-05 18:31:38 +0300

    Redux_Quiz

--- my-app/src/store/actions/quiz.js
+++ my-app/src/store/actions/quiz.js
@@ -1,28 +1,54 @@
 import  axios from "../../axios/axios-quiz";
-import {FETCH_QUIZES_START, FETCH_QUIZES_ERROR, FETCH_QUIZES_SUCCESS} from "./actionTypes";
-
-
-
+import {
+    FETCH_QUIZES_START,
+    FETCH_QUIZES_ERROR,
+    FETCH_QUIZES_SUCCESS,
+    FETCH_QUIZ_SUCCESS,
+    QUIZ_SET_STATE, FINISH_OUIZ, QUIZ_NEXT_QUESTION, QUIZ_RETRY
+} from "./actionTypes";
 
 export default function fetchQuizes() {
     return async dispatch => {
         dispatch(fetchQuizesStart());
         try {
             const response = await axios.get('/quizes.json')
-
             const quizes = [];
+
             Object.keys(response.data).forEach((key, index) => {
                 quizes.push({
                    id:key,
                    name:`Тест №${index+1}`
                })
-            })
+            });
             dispatch(fetchQuizesSucccess(quizes))
         }catch (e) {
             dispatch(fetchQuizesError(e))
         }
     }
 }
+
+export function fetchQuizSucccess(quiz) {
+    return {
+        type: FETCH_QUIZ_SUCCESS,
+        quiz
+    }
+}
+
+export function fetchQuizById () {
+    return async dispatch => {
+        dispatch(fetchQuizesStart());
+        try {
+            const response = await axios.get(`${window.location.pathname}.json`);
+            const quiz = response.data;
+
+            dispatch(fetchQuizSucccess(quiz))
+        }catch (e) {
+
+            dispatch(fetchQuizesError(e))
+        }
+    }
+}
+
 export function fetchQuizesStart() {
     return {
         type: FETCH_QUIZES_START
@@ -38,5 +64,70 @@ export function fetchQuizesError(e) {
     return {
         type: FETCH_QUIZES_ERROR,
         error: e
+    }
+}
+
+function quizSetState(answerState, results) {
+    return{
+        type: QUIZ_SET_STATE,
+        answerState, results
+    }
+}
+
+function finishQuiz() {
+    return{
+        type: FINISH_OUIZ
+    }
+
+}
+
+function quizNextQuestion(number) {
+    return{
+        type: QUIZ_NEXT_QUESTION,
+        number
+    }
+}
+
+export function quizAnswerClick(answerId) {
+    return (dispatch, getState) =>{
+        const state = getState().quiz;
+        if (state.answerState) {
+            const key = Object.keys(state.answerState)[0];
+            if (state.answerState[key] === 'success') {
+                return
+            }
+        }
+
+        const question = state.quiz[state.activeQuestion];
+        const results = state.results;
+
+        if (question.rightAnswerId === answerId) {
+            if (!results[question.id]) {
+                results[question.id] = 'success'
+            }
+            dispatch(quizSetState({[answerId]: 'success'},results))
+
+            const timeout = window.setTimeout(() => {
+                if (isQuizFinished(state)) {
+                    dispatch(finishQuiz())
+
+                } else {
+                    dispatch(quizNextQuestion(state.activeQuestion + 1))
+                }
+                window.clearTimeout(timeout)
+            }, 1000)
+        } else {
+            results[question.id] = 'error';
+            dispatch(quizSetState({[answerId]: 'error'},results))
+        }
+    }
+}
+function isQuizFinished(state) {
+    return state.activeQuestion + 1 === state.quiz.length
+}
+
+export function retryQuiz(){
+    return{
+        type: QUIZ_RETRY
     }
 }
